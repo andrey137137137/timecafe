1e9d4512ad65b7266244ef92ca893928b56916e9
Max Matiukhin <matuhinmax@mail.ru>
2018-08-07 21:29:08 +0300

    Перетянул языковой файлик с сервера

--- frontend/modules/visits/model/StartVisit.php
+++ frontend/modules/visits/model/StartVisit.php
@@ -8,13 +8,13 @@ use yii\base\Model;
 class StartVisit extends \yii\db\ActiveRecord
 {
 
-  public $f_name;
+  /*public $f_name;
   public $l_name;
   public $id;
   public $lg;
   public $code;
   public $phone;
-  public $email;
+  public $email;*/
   public $type=false;
 
   //таблица задана что б проверить уникальность полей
@@ -52,7 +52,18 @@ class StartVisit extends \yii\db\ActiveRecord
         [['f_name', 'l_name', 'code', 'email', 'phone','lg'], 'string'],
         [[ 'id','type'], 'integer'],
         [[ 'email'], 'email'],
-        [[ 'code','email','phone'], 'unique'],
+        [[ 'code','email','phone'], 'unique','filter' =>function($model) {
+          return $model;
+          if($model->type==2){
+            return "id!=".$model->id;
+          }
+          return "1==1";
+        },'when' => function($model) {
+          return $model->type > 0;
+        }],
+        //[[ 'code'], 'unique', 'targetAttribute' => ['code', 'id']],
+        //[[ 'email'], 'unique', 'targetAttribute' => ['email']],
+        //[[ 'phone'], 'unique', 'targetAttribute' => ['phone', 'id']],
         [['f_name'],"required",'when' => function($model) {
           return $model->type > 0;
         }]
