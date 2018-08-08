7ec3f71154efa84a1266a704196ea49860671b91
Max Matiukhin <matuhinmax@mail.ru>
2018-08-08 13:35:56 +0300

    С сервера от Артура

--- common/language/en-EN/main.php
+++ common/language/en-EN/main.php
@@ -9,13 +9,12 @@
   'Data visitors' => 'Data visitors',
   'Start new visit' => 'Start new visit',
   'Find visitor or enter first name.' => 'Find visitor or enter first name.',
-  'ajax_find_line' => '<p>
-                        <span><i class="fa fa-user"></i> : {{f_name}} {{l_name}}</span>
-                       <span class="ppe"><i class="fa fa-envelope"></i> : {{email}}</span>
-                       <span><i class="fa fa-phone"></i> : {{phone}}</span>
-                       <span><b>ID</b> : {{code}}</span>
-                       </p>',
+  'ajax_find_line' => '<div class="row">
+                        <div class="col-md-6"><i class="fa fa-user"></i> : {{f_name}} {{l_name}}</div>
+						 <div class="col-md-6"><b>ID</b> : {{code}}</div>
+                         <div class="col-md-6"><i class="fa fa-envelope"></i> : {{email}}</div>
+						 <div class="col-md-6"><i class="fa fa-phone"></i> : {{phone}}</div>
+                       </div>',
   'Unable to find repositories for selected.' => 'Unable to find repositories for selected.',
-  'Visitors' => 'Visitors',
 );
 ?>
--- common/language/ru-RU/main.php
+++ common/language/ru-RU/main.php
@@ -7,5 +7,15 @@ return array (
     'System management' => 'Управление системой',
     'Add customer' => 'Добавить клиента',
     'Data visitors' => 'Клиенты',
+	'Start new visit' => 'Старт нового визита',
+	'Find visitor or enter first name.' => 'Найдите посетителя или введите имя.',
+	'ajax_find_line' => '<p>
+                        <span><i class="fa fa-user"></i> : {{f_name}} {{l_name}}</span>
+                       <span class="ppe"><i class="fa fa-envelope"></i> : {{email}}</span>
+                       <span><i class="fa fa-phone"></i> : {{phone}}</span>
+                       <span><b>ID</b> : {{code}}</span>
+                       </p>',
+	'Unable to find repositories for selected.' => 'Не удалось найти ранее сохранённых данных введённого.',
+  
 );
 ?>
