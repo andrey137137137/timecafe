35ada57aad2f4ee0f4d69ab5e942538aedf02fc4
Andrey Bannikov <andrey27x777@gmail.com>
2018-08-16 17:25:50 +0300

    Edited model Cafe.php

--- frontend/config/main-local.example.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'components' => [
-        'request' => [
-            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'BHi6c8NuEzPG6HJ66edgEC70ANtO6haX',
-        ],
-    ],
-];
